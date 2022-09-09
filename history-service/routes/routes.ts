c0ab49061f5740019ea0e926946251862db21c1a
seanjyjy <e0406739@u.nus.edu>
2022-09-10 02:52:01 +0800

    add unsaved code

--- history-service/routes/routes.ts
+++ history-service/routes/routes.ts
@@ -1,9 +1,9 @@
 import express from 'express';
-import { createHistory, updateUserHistory, getAllHistory, getUserHistory } from '../service/history-service';
+import { createHistory, updateUserHistory, getAllHistory, getUserHistory } from '../controller/history-controller';
 
 const router = express.Router();
 
-router.route('/').get(getStudents).post(createStudent).put(errorThrowForUnsupportedApi).delete(errorThrowForUnsupportedApi);
-router.route('/:id').put(updateStudent).delete(deleteStudent).get(getStudent).post(errorThrowForUnsupportedApi);
+router.route('/').get(getAllHistory);
+router.route('/:username').get(getUserHistory).put(updateUserHistory).post(createHistory);
 
 export default router;
