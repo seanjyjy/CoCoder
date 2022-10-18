8633ca01958f1ce87fbc61bc1f654795b829e030
lingshanng <nglingshan456@gmail.com>
2022-10-18 17:07:21 +0800

    add history svc and separate redis

--- start.bat
+++ start.bat
@@ -4,10 +4,14 @@ kind load docker-image pp-user --name kind-1
 kind load docker-image pp-matching --name kind-1
 kind load docker-image pp-collaboration --name kind-1
 kind load docker-image pp-question --name kind-1
+kind load docker-image pp-history --name kind-1
 kubectl create secret generic user-svc-secrets --from-env-file ./user-service/.env
+kubectl create secret generic history-svc-secrets --from-env-file ./history-service/.env
 kubectl apply -f k8s/manifests/redis-matching-deployment.yaml
+kubectl apply -f k8s/manifests/redis-collab-deployment.yaml
 kubectl apply -f k8s/manifests/user-deployment.yaml
 kubectl apply -f k8s/manifests/question-deployment.yaml
+kubectl apply -f k8s/manifests/history-deployment.yaml
 kubectl apply -f k8s/manifests/collaboration-deployment.yaml
 kubectl apply -f k8s/manifests/matching-deployment.yaml
 kubectl apply -f k8s/manifests/frontend-deployment.yaml
